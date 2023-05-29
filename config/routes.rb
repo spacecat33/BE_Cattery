ecd01031d13defde0093c84de73e99730ce8db4d
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-29 12:18:11 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
-
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 end
