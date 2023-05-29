27f0327e6e4661bf780764018c0053c1cde67214
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-29 12:07:38 +0100

    added user and auth controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+15.times do
+    User.create(
+      username: Faker::Name.unique.name, password: 'hi', avatar: "https://images.unsplash.com/photo-1601979031925-424e53b6caaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHVwcHl8ZW58MHx8MHx8fDA%3D&w=1000&q=80", bio: "Hello I am a wannabe Cat!"
+    )
+  end
